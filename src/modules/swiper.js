9b4f06653f88d7ec52cedba705a5feb9fd285d18
HaroldSP <george.st.petersburg@gmail.com>
2023-03-22 16:15:40 +0300

    fix (first slider swiper width and pagination)

--- src/modules/swiper.js
+++ src/modules/swiper.js
@@ -25,10 +25,7 @@ const initSwiper = () => {
     },
     pagination: {
       el: '.swiper-pagination',
-      clickable: true,
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (index + 1) + '</span>';
-      }
+      clickable: true
     }
   });
 
